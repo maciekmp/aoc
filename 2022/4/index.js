f52f8e23f871e06ea65cd86b95016533a31f4a70
Maciej Płusa <maciekmp@gmail.com>
2022-12-04 11:42:53 +0100

    day 4 part 2

--- 2022/4/index.js
+++ 2022/4/index.js
@@ -17,14 +17,30 @@ fs.readFile('input.txt', 'utf8', (err, data) => {
         const left = parts[0].split('-');
         const right = parts[1].split('-');
         // right inside of left
-        if (right[0] >= left[0] && right[1] <= left[1]) {
+        if (+right[0] >= +left[0] && +right[1] <= +left[1]) {
             return 1;
         }
         // left inside of right
-        if (left[0] >= right[0] && left[1] <= right[1]) {
+        if (+left[0] >= +right[0] && +left[1] <= +right[1]) {
             return 1;
         }
         return 0;
     })
     console.log(1, sum(values));
+    const values2 = array.map(line => {
+        if (line.length === 0) {
+            return 0;
+        }
+        const parts = line.split(',')
+        const left = parts[0].split('-');
+        const right = parts[1].split('-');
+        if (+right[0] <= +left[1] && +right[0] >= +left[0]) {
+            return 1;
+        }
+        if (+right[1] <= +left[1] && +right[1] >= +left[0]) {
+            return 1;
+        }
+        return 0;
+    })
+    console.log(2, sum(values2));
 });
