4e7d5b5ac1bca5f7536f4f22a46592f85517ecfb
Maciej Płusa <maciekmp@gmail.com>
2022-12-04 10:54:39 +0100

    day 4

---
+++ 2022/4/index.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+
+const sum = arr => arr.reduce((prev, cur) => prev + cur);
+
+fs.readFile('input.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    const array = data.split('\n');
+    const values = array.map(line => {
+        if (line.length === 0) {
+            return 0;
+        }
+        const parts = line.split(',')
+        const left = parts[0].split('-');
+        const right = parts[1].split('-');
+        // right inside of left
+        if (right[0] >= left[0] && right[1] <= left[1]) {
+            return 1;
+        }
+        // left inside of right
+        if (left[0] >= right[0] && left[1] <= right[1]) {
+            return 1;
+        }
+        return 0;
+    })
+    console.log(1, sum(values));
+});
